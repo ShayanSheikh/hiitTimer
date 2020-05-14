4acddfd3f05c5e2fc12148af03973b4d5142a78b
Shayan Sheikh <najam.shayan@gmail.com>
2020-05-14 14:51:17 -0400

    Timer formatting and running

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,51 @@ import React, { useState } from 'react';
 import { Input } from 'semantic-ui-react';
 
 const TimerApp = () => {
-  const [time, setTime] = useState('0');
+  const [countdownTime, setCountdownTime] = useState('');
+  const [timerRunning, setTimerRunning] = useState(false);
+
+  const convertTimeToSeconds = time => {
+    let minutes = parseInt(time.slice(0, 2)) * 60;
+    let seconds = parseInt(time.slice(2));
+
+    return minutes + seconds;
+  };
 
   const startTimer = () => {
-    setInterval(function () {
-      setTime(time - 1);
-    }, 1000);
+    setTimerRunning(true);
+    const start = Date.now()
+    let diff, minutes, seconds;
+
+    let countdownTimeSeconds = convertTimeToSeconds(countdownTime);
+
+    const countdownInterval = setInterval(() => {
+      diff = countdownTimeSeconds - (((Date.now() - start) / 1000) | 0);
+
+      minutes = (diff / 60) | 0;
+      seconds = (diff % 60) | 0;
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      setCountdownTime(`${minutes}:${seconds}`);
+
+      if (diff <= 0) {
+        setTimerRunning(false);
+        clearInterval(countdownInterval);
+      }
+    });
   }
+
+  const timeFormatter = time => {
+    console.log(time)
+    while (time.length < 4) {
+      time = '0' + time;
+    };
+    let minutes = time.slice(0,2);
+    let seconds = time.slice(2);
+
+    return `${minutes}:${seconds}`
+  };
 
   return (
     <>
@@ -17,13 +55,11 @@ const TimerApp = () => {
         action={{
           color: 'blue',
           content: 'Start',
-          onClick: () => setInterval(() => {
-            setTime(time - 1);
-          }, 1000)
+          onClick: () => startTimer()
         }}
-        placeholder='Enter Time'
-        onChange={(e)=> setTime(e.target.value)}
-        value={time}
+        placeholder='00:00'
+        onChange={(e) => setCountdownTime(e.target.value.replace(/:+/gm, '').slice(-4))}
+        value={timerRunning ? countdownTime : timeFormatter(countdownTime)}
       />
     </>
   )
